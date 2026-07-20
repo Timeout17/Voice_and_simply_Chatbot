3efa787491200f828388f9322e1bb8bb62014353
Csaba Attila <h587764@stud.u-szeged.hu>
2026-07-20 16:27:29 +0200

    Make the Connention, and Initializer classes

---
+++ Project/src/data/DatabaseInitializer.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class DatabaseInitializerClass():
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def CreateMessagetable(self):
+        try:
+            cur = self.conn.cursor()
+            
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS messages(
+                    id TEXT PRIMARY KEY,
+                    prompt TEXT,
+                    role TEXT,
+                    time NUMERIC
+                );
+                """
+            )
+
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(e)
+            # itt lesz még loggolás
