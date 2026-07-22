f6ad729a3fd59eebe1ea270025ea76b68c65e355
Csaba Attila <h587764@stud.u-szeged.hu>
2026-07-22 11:05:35 +0200

    I put back what had been working before

---
+++ Project/src/data/DataBaseConnention.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pathlib import Path
+
+
+
+class DataBaseConnentionClass():
+    
+    def __init__(self):
+        self.db_path: Path = Path(__file__).parent.parent.parent / "database" / "chatbot_data.sqlite"
+        self.db_path.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+    def get_connection(self) -> sqlite3.Connection:
+        conn = sqlite3.connect(self.db_path)
+        conn.row_factory = sqlite3.Row
+
+        return conn
+    
+    def close(self, conn: sqlite3.Connection):
+        if conn:
+            conn.close()
+
